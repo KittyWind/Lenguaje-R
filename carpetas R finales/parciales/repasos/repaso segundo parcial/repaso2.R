9a10257ccf6e4a1340ca97a23d145bad7c72ca5f
Kitty <nicolasasperatti@gmail.com>
2024-10-29 14:04:35 -0300

    escritas conignas

---
+++ carpetas R finales/parciales/repasos/repaso segundo parcial/repaso2.R
@@ -0,0 +1,16 @@
+# Ejercicios de práctica
+# 1) Utilizar el dataset Faithful de R base y analizar si las variables continuas se distribuyen
+# normalmente. Justificar.
+# 2) Utilizar el dataset CO2 de R base y analizar si las variables continuas se distribuyen
+# normalmente. Justificar.
+# 3) Estimar puntualmente la media y el desvío de las variables waiting (Faithful)
+# 4) Estimar puntualmente la media y el desvìo de la variable uptake (CO2)
+# 5) Construir un intervalo de confianza de nivel de 95% para la media de la variable
+# waiting. Interpretar.
+# 6) Construir un intervalo de confianza de nivel de 98% para la media de la variable
+# uptake. Interpretar.
+# 7) Realizar un análisis de regresión lineal completo (modelo, bondad del modelo y alguna predicción) para cada situación (base CO2)
+# Situación a) concentración del dióxido de carbono en función de la tasa de absorción.
+# Situación b) concentración del dióxido de carbono en función de la tasa de absorción y
+# el tratamiento aplicado a la planta.
+# En todas las situaciones, interpretar.
